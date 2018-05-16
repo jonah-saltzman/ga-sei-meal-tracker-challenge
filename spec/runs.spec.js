77163a07b2e8208ebe114d9b1098f94b1831adcc
cpearce31 <cpearce31@gmail.com>
2018-05-16 16:28:03 -0400

    Improve tests
    De-couple test data from the `lib` files using `.call` and add tests for the
    meals lab.
    
    Close #11. Close #33.

--- spec/runs.spec.js
+++ spec/runs.spec.js
@@ -6,21 +6,36 @@ const expect = chai.expect
 const runs = require('../lib/runs')
 
 describe('runs', function () {
+  const testUser = {
+    runs: [
+      {
+        date: '2017-05-25 15:00',
+        distance: 2300,
+        timeTaken: 800
+      },
+      {
+        date: '2017-05-25 15:00',
+        distance: 1700,
+        timeTaken: 1200
+      }
+    ]
+  }
+
   describe('total distance', function () {
     it('has the correct length', function () {
-      expect(runs.totalDistance()).to.equal(2600)
+      expect(runs.totalDistance.call(testUser)).to.equal(400)
     })
   })
 
   describe('longest distance run', function () {
     it('has the correct length', function () {
-      expect(runs.longestRunDistance()).to.equal(1400)
+      expect(runs.longestRunDistance.call(testUser)).to.equal(2300)
     })
   })
 
   describe('average speed', function () {
     it('has the correct value', function () {
-      expect(runs.averageSpeed().toLocaleString('en-US')).to.equal('1.857')
+      expect(runs.averageSpeed.call(testUser)).to.equal(2)
     })
   })
 })
